5e1896814f1e186bfd2b119487e834af9829684f
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-02-15 07:49:42 +0200

    Update config.py

--- forwarder/config.py
+++ forwarder/config.py
@@ -2,12 +2,12 @@ from forwarder.sample_config import Config
 
 
 class Development(Config):
-    API_KEY = "5936802228:AAFwW7cZlF8waTYTyVe8m3nUYhJg5EF9Eso"  # Your bot API key
+    API_KEY = "5881051237:AAGS8hiOBdxPxgiYu9GOQn07uI6qBrft6f0"  # Your bot API key
     OWNER_ID = 1227193881  # Your user id
 
     # Make sure to include the '-' sign in group and channel ids.
-    FROM_CHATS = [1227193881]  # List of chat id's to forward messages from.
-    TO_CHATS = [-1001739160183]  # List of chat id's to forward messages to.
+    FROM_CHATS = [1227193881, 6297395845]  # List of chat id's to forward messages from.
+    TO_CHATS = [-1001866642464]  # List of chat id's to forward messages to.
 
     REMOVE_TAG = True
     WORKERS = 4
